89052a9c1ab08a660e230f75feac54dacd3f18fe
SardarkhanStella <skhan@stellatechnology.com>
2023-08-02 16:31:58 +0500

    We have passed dependencies from UI class.

--- app/src/main/java/com/traiden/manualdependencyinjection/MainActivity.kt
+++ app/src/main/java/com/traiden/manualdependencyinjection/MainActivity.kt
@@ -2,10 +2,18 @@ package com.traiden.manualdependencyinjection
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.traiden.manualdependencyinjection.di.dagger.EmailService
+import com.traiden.manualdependencyinjection.di.dagger.UserRegistration
+import com.traiden.manualdependencyinjection.di.dagger.UserRepo
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val userRepo = UserRepo()
+        val emailService = EmailService()
+        val userRegistration = UserRegistration(userRepo,emailService)
+        userRegistration.registerUser("test@gmail.com","123")
+
     }
 }
