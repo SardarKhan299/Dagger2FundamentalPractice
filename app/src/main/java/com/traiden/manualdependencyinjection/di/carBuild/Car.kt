1b9cfc477538951e5d2f14a60e3b3d054a8bab91
SardarkhanStella <skhan@stellatechnology.com>
2023-08-02 14:59:41 +0500

    Introduce constructor injection.

--- app/src/main/java/com/traiden/manualdependencyinjection/di/carBuild/Car.kt
+++ app/src/main/java/com/traiden/manualdependencyinjection/di/carBuild/Car.kt
@@ -1,14 +1,23 @@
 package com.traiden.manualdependencyinjection.di.carBuild
 
-class Car {
+// constructor injection..//
+class Car(private val engine1:Engine) {
 
     // At that moment our Car class has some problems.
-    // it cannot be unit test separatly.//
+    // it cannot be unit test properly.//
+    // code is not extensible..// because our class is dependent on this engine. we cannot add more engines.
+    // Voilates Single Responsibility.
+    // LifeTime of objects. Engine class object lifetime is dependent on car.//
+    // what if we need to replace the engine. When our car object is destroyed then our engine object is destroyed..//
+    // we cannot reuse this engine Object. we cannot use that engine object in another class..//
+    // system will provide the dependencies..our class will not create the dependencies...//
+
 
     private var engine:Engine = Engine()
 
     fun startCar(){
         engine.start()
+        engine1.start()
     }
 
 }
