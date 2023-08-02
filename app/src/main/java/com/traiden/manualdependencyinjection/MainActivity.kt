d7f886b40c502f550e95fa95d7193f6de1d5f81f
SardarkhanStella <skhan@stellatechnology.com>
2023-08-02 18:58:01 +0500

    instead of using methods to get the object for every object. you can use inject annotation and use one method in componenet and pass the consumer.

--- app/src/main/java/com/traiden/manualdependencyinjection/MainActivity.kt
+++ app/src/main/java/com/traiden/manualdependencyinjection/MainActivity.kt
@@ -7,19 +7,28 @@ import com.traiden.manualdependencyinjection.di.dagger.EmailService
 import com.traiden.manualdependencyinjection.di.dagger.UserRegistration
 import com.traiden.manualdependencyinjection.di.dagger.UserRegistrationComponent
 import com.traiden.manualdependencyinjection.di.dagger.UserRepo
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var userRegistration: UserRegistration
+
+    @Inject
+    lateinit var emailService: EmailService
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
         val component = DaggerUserRegistrationComponent.builder().build()
+        component.inject(this)
 //        val userRepo = UserRepo()
 //        val emailService = EmailService()
 //        val userRegistration = UserRegistration(userRepo,emailService)
-        val emailService = component.getEmailService()
-        component.getUserRegistrationService().registerUser("test@gmail.com","123")
+//        val emailService = component.getEmailService()
+        userRegistration.registerUser("test@gmail.com","123")
 
     }
 }
--- app/src/main/java/com/traiden/manualdependencyinjection/di/dagger/UserRegistrationComponent.kt
+++ app/src/main/java/com/traiden/manualdependencyinjection/di/dagger/UserRegistrationComponent.kt
@@ -1,11 +1,14 @@
 package com.traiden.manualdependencyinjection.di.dagger
 
+import com.traiden.manualdependencyinjection.MainActivity
 import dagger.Component
 
 @Component
 interface UserRegistrationComponent {
 
-   fun  getUserRegistrationService():UserRegistration
-   fun getEmailService():EmailService
+//   fun  getUserRegistrationService():UserRegistration
+//   fun getEmailService():EmailService
+
+   fun inject(mainActivity: MainActivity)
 
 }
